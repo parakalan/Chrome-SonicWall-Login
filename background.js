a7cc188481914b348ad83c72e146c52e50d29f7e
S T Sudharsan <stsudharshan@gmail.com>
2017-10-31 14:41:50 +0530

    Handle INSECURE_RESPONSE and ADDRESS_UNREACHABLE

--- background.js
+++ background.js
@@ -8,6 +8,23 @@ function loadScript(scriptName, callback) {
 loadScript('popup');
 loadScript('jquery.min')
 chrome.alarms.onAlarm.addListener(function(alarm) {
-	console.log('Alarm running');
-	postUsrHeartbeat();
+    console.log('Alarm running');
+    postUsrHeartbeat();
+});
+
+chrome.webRequest.onErrorOccurred.addListener(function(details) {
+        $('.loader').hide();
+        if (details.error == "net::ERR_INSECURE_RESPONSE")
+        {
+            $('.info').html("Chrome's Insecure Reponse. Go to https://192.168.20.1 and select proceed anyway");
+        }
+        else if(details.error == "net::ERR_ADDRESS_UNREACHABLE")
+        {
+            $('.info').html("You're not connected to HP / NITT Wifi.");
+
+        }
+    },
+    {
+        urls: ['*://*/*'],
+        types: ['xmlhttprequest']
 });
